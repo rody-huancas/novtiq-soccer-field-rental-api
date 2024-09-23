a9ee02927b0967b6c01f15d4adee6443f6d40e00
Rody Huancas <rodyhuancas.04@gmail.com>
2024-09-22 19:05:34 -0500

    Feat: transactions in permissions

--- src/modules/roles/permissions/permissions.service.ts
+++ src/modules/roles/permissions/permissions.service.ts
@@ -1,5 +1,5 @@
-import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 /* DTOs */
 import { CreatePermissionDto } from './dto/create-permission.dto';
@@ -7,26 +7,37 @@ import { UpdatePermissionDto } from './dto/update-permission.dto';
 /* Entities */
 import { Permission } from './entities/permission.entity';
 /* Utils */
-import { checkExistence } from '@root/src/utils/checkExistence';
+import { checkExistence } from '@utils/checkExistence';
+import { validateExistence } from '@utils/validateExistence';
 
 @Injectable()
 export class PermissionsService {
   constructor(
     @InjectRepository(Permission) private readonly permissionRepository: Repository<Permission>,
+    private dataSource: DataSource
   ) {}
 
   async create(createPermissionDto: CreatePermissionDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
       const { pe_name, ...data } = createPermissionDto;
 
       /* Verificar si el nombre del permiso ya existe */
       await checkExistence(this.permissionRepository, 'pe_name', pe_name);
       
-      const newPermission = await this.permissionRepository.save({ ...data, pe_name });
+      await queryRunner.manager.save(Permission, { ...data, pe_name });
       
-      return newPermission;
+      await queryRunner.commitTransaction();
+
+      return { message: `El permiso '${pe_name}' ha sido creado correctamente.` };
     } catch (error) {
+      await queryRunner.rollbackTransaction();
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    } finally {
+      await queryRunner.release();
     }
   }
 
@@ -41,10 +52,7 @@ export class PermissionsService {
 
   async findOne(id: string) {
     try {
-      const permission = await this.permissionRepository.findOne({ where: { pe_id: id } });
-      if (!permission) {
-        throw new HttpException(`Permiso con id '${id}' no encontrado.`, HttpStatus.NOT_FOUND);
-      }
+      const permission = await validateExistence(this.permissionRepository, id, 'pe_id', 'Permiso');
       return permission;
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
@@ -52,27 +60,48 @@ export class PermissionsService {
   }
 
   async update(id: string, updatePermissionDto: UpdatePermissionDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
+      const permission = await this.findOne(id);
+
       const { pe_name, ...data } = updatePermissionDto;
 
       /* Verificar si el nombre del permiso ya existe */
-      await checkExistence(this.permissionRepository, 'pe_name', pe_name);
+      if (pe_name !== permission.pe_name) await checkExistence(this.permissionRepository, 'pe_name', pe_name);
+
+      const permissionUpdated = await this.permissionRepository.preload({ pe_id: id, ...data, pe_name });
+
+      await queryRunner.manager.save(permissionUpdated);
+      await queryRunner.commitTransaction();
       
-      const updatedPermission = await this.permissionRepository.update(id, { ...data, pe_name });
-      
-      return updatedPermission;
+      return { message: `El permiso '${permissionUpdated.pe_name}' ha sido actualizado correctamente.` };
     } catch (error) {
+      await queryRunner.rollbackTransaction();
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    } finally {
+      await queryRunner.release();
     }
   }
 
   async remove(id: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
       const permission = await this.findOne(id);
-      await this.permissionRepository.delete(id);
+      await queryRunner.manager.delete(Permission, id);
+      
+      await queryRunner.commitTransaction();
       return { message: `El permiso '${permission.pe_name}' ha sido eliminado correctamente.` };
     } catch (error) {
+      await queryRunner.rollbackTransaction();
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    } finally {
+      await queryRunner.release();
     }
   }
 }
